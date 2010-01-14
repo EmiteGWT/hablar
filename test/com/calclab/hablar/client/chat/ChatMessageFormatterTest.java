39074432cb7b3e050c228360b306283f36fedcd9
vruiz.jurado@gmail.com <vruiz.jurado@gmail.com@87f8d446-a148-0410-9e96-a52f46714620>
2010-01-14 13:19:42 +0000

    Tests added/removed
    git-svn-id: https://emite.googlecode.com/svn/trunk/hablar@1037 87f8d446-a148-0410-9e96-a52f46714620

--- test/com/calclab/hablar/client/chat/ChatMessageFormatterTest.java
+++ test/com/calclab/hablar/client/chat/ChatMessageFormatterTest.java
@@ -1,5 +1,6 @@
 package com.calclab.hablar.client.chat;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -7,18 +8,25 @@ import org.junit.Test;
 public class ChatMessageFormatterTest {
 
     @Test
-    public void shouldFormatURL() {
-	final String[] urls = new String[] { "http://emite.googlecode.com/", "ftp://debian.org/",
-		"http://www.google.com/search?hl=es&rls=GGGL%2CGGGL%3A2006-28%2CGGGL%3Aes&q=emite&btnG=Buscar&lr=",
-		"http://del.icio.us/search/?fr=del_icio_us&p=xmpp+gwt&type=all" };
+    public void formatNewlines() {
+	assertEquals("first line<br>\nsecond line<br>\n", ChatMessageFormatter.format("first line\nsecond line\n"));
+    }
+
+    @Test
+    public void formatURLs() {
+	final String[] urls = new String[] { "http://emite.googlecode.com/", "ftp://debian.org/"
+	// commented while we find a way to sanitizing URLs and prevent CSS
+	// "http://www.google.com/search?hl=es&rls=GGGL%2CGGGL%3A2006-28%2CGGGL%3Aes&q=emite&btnG=Buscar&lr=",
+	// "http://del.icio.us/search/?fr=del_icio_us&p=xmpp+gwt&type=all"
+	};
 	for (final String url : urls) {
-	    String result = ChatMessageFormatter.format(url);
+	    final String result = ChatMessageFormatter.format(url);
 	    assertContains(result, "<a");
 	    assertContains(result, "href=\"" + url + "\"");
 	}
     }
 
-    private void assertContains(String result, String expected) {
+    private void assertContains(final String result, final String expected) {
 	assertTrue("Expected " + result + " to contain " + expected, result.indexOf(expected) > -1);
     }
 }
