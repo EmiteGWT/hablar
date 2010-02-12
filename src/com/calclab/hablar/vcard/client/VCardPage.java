fa3c7d9c5733b927c35bcfa3be247ea055a94957
vruiz.jurado <vruiz.jurado@87f8d446-a148-0410-9e96-a52f46714620>
2010-02-12 03:22:03 +0000

    VCardPage updated
    git-svn-id: https://emite.googlecode.com/svn/trunk/hablar@1176 87f8d446-a148-0410-9e96-a52f46714620

--- src/com/calclab/hablar/vcard/client/VCardPage.java
+++ src/com/calclab/hablar/vcard/client/VCardPage.java
@@ -15,31 +15,58 @@ public class VCardPage extends PagePresenter<VCardDisplay> implements EditorPage
     public static final String TYPE = "VCard";
     private static int id = 0;
 
-    public VCardPage(HablarEventBus eventBus, VCardDisplay display) {
+    public VCardPage(final HablarEventBus eventBus, final VCardDisplay display) {
 	super(TYPE, "" + (++id), eventBus, display);
 	model.init(HablarIcons.get(IconType.buddyWait), "User profile");
     }
 
     @Override
     public void saveData() {
-	// should save vcard here
-	// String nickname = display.getNickName().getText();
-	// etc...
+	final VCardManager manager = Suco.get(VCardManager.class);
+	// It's a update, then first of all we get the current own vcard
+	manager.requestOwnVCard(new Listener<VCardResponse>() {
+	    @Override
+	    public void onEvent(final VCardResponse response) {
+		if (response.hasVCard()) {
+		    final VCard vcard = response.getVCard();
+		    // should save vcard here
+		    // String nickname = display.getNickName().getText();
+		    // etc...
+		    // String nickname = display.getNickName().getText();
+
+		    // We need vcard.set methods here
+		    manager.updateOwnVCard(vcard, new Listener<VCardResponse>() {
+			@Override
+			public void onEvent(final VCardResponse response) {
+			    update(response);
+			}
+		    });
+		}
+	    }
+	});
     }
 
     @Override
     public void showData() {
 	// should load and display vcard here
-	VCardManager manager = Suco.get(VCardManager.class);
+	final VCardManager manager = Suco.get(VCardManager.class);
 	manager.requestOwnVCard(new Listener<VCardResponse>() {
 	    @Override
-	    public void onEvent(VCardResponse response) {
-		if (response.hasVCard()) {
-		    VCard vcard = response.getVCard();
-		    display.getNickName().setText(vcard.getNickName());
-		    // etc...
-		}
+	    public void onEvent(final VCardResponse response) {
+		update(response);
 	    }
 	});
+    }
+
+    private void update(final VCardResponse response) {
+	if (response.hasVCard()) {
+	    final VCard vcard = response.getVCard();
+	    display.getEmail().setText(vcard.getJabberID());
+	    display.getFirstName().setText(vcard.getGivenName());
+	    display.getMiddleName().setText(vcard.getMiddleName());
+	    display.getNickName().setText(vcard.getNickName());
+	    display.getOrganizationName().setText(vcard.getOrganization().getName());
+	    display.getSurname().setText(vcard.getFamilyName());
+	}
     }
 }
