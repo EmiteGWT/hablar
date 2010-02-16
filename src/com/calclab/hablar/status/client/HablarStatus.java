4e7e2416216ae0c4e7215bb07ba082e46840924f
danigb <danigb@87f8d446-a148-0410-9e96-a52f46714620>
2010-02-16 01:10:16 +0000

    All modules with correct dependencies. GWT succeed
    git-svn-id: https://emite.googlecode.com/svn/trunk/hablar@1230 87f8d446-a148-0410-9e96-a52f46714620

---
+++ src/com/calclab/hablar/status/client/HablarStatus.java
@@ -0,0 +1,14 @@
+package com.calclab.hablar.status.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+/**
+ * Handle ChatState state for both chat and rooms
+ */
+public class HablarStatus implements EntryPoint {
+
+    @Override
+    public void onModuleLoad() {
+    }
+
+}
