ea24f0f629ea7df7cc1e892b1c74552cd1fda523
vruiz.jurado <vruiz.jurado@87f8d446-a148-0410-9e96-a52f46714620>
2010-03-03 20:21:25 +0000

    selenium roster tests
    git-svn-id: https://emite.googlecode.com/svn/trunk/hablar@1339 87f8d446-a148-0410-9e96-a52f46714620

--- test/com/calclab/hablar/selenium/editbuddy/EditBuddyPageSeleniumTests.java
+++ test/com/calclab/hablar/selenium/editbuddy/EditBuddyPageSeleniumTests.java
@@ -14,6 +14,7 @@ public class EditBuddyPageSeleniumTests extends AbstractRosterTests {
 	roster.getItemMenu("", "selenium@localhost").click();
 	roster.getEditBuddyAction().click();
 	// mustCloseFinally = false;
+	removeSeleniumBuddy();
 	logout();
     }
 
