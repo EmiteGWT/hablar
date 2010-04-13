0cb674249db9cba918471f2e683876b3a524a75d
danigb <danigb@87f8d446-a148-0410-9e96-a52f46714620>
2010-04-13 16:03:07 +0000

    Nick name typo. Fixes issue 244
    git-svn-id: https://emite.googlecode.com/svn/trunk/hablar@1400 87f8d446-a148-0410-9e96-a52f46714620

--- src/com/calclab/hablar/editbuddy/client/EditBuddyMessages.java
+++ src/com/calclab/hablar/editbuddy/client/EditBuddyMessages.java
@@ -5,27 +5,27 @@ import com.google.gwt.i18n.client.LocalizableResource.DefaultLocale;
 
 @DefaultLocale("en")
 public interface EditBuddyMessages extends Messages {
-    @DefaultMessage("Change nick name")
-    String changeNickName();
-
-    @DefaultMessage("Change buddy nickname")
-    String changeBuddyNickname();
-
-    @DefaultMessage("Write the new nick name:")
-    String changeNickNameMessage();
-
-    @DefaultMessage("Nick name can''t be empty")
-    String nickNameEmpty();
-
-    @DefaultMessage("Change from:")
-    String changeFromLabelText();
-
-    @DefaultMessage("To:")
-    String changeToLabelText();
-
     @DefaultMessage("Change")
     String acceptAction();
 
     @DefaultMessage("Change")
     String cancelAction();
+
+    @DefaultMessage("Change buddy nickname")
+    String changeBuddyNickname();
+
+    @DefaultMessage("Change from:")
+    String changeFromLabelText();
+
+    @DefaultMessage("Change nickname")
+    String changeNickName();
+
+    @DefaultMessage("Write the new nickname:")
+    String changeNickNameMessage();
+
+    @DefaultMessage("To:")
+    String changeToLabelText();
+
+    @DefaultMessage("Nick name can''t be empty")
+    String nickNameEmpty();
 }
