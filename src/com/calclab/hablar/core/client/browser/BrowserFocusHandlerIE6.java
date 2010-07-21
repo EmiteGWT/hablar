9aba1d8fcf120f6dee75bb4eb37198d83f03e15a
ashward.surevine <ashward.surevine@87f8d446-a148-0410-9e96-a52f46714620>
2010-07-21 15:55:39 +0000

    Fixed BrowserFocusHandler in IE6
    git-svn-id: https://emite.googlecode.com/svn/trunk/hablar@1524 87f8d446-a148-0410-9e96-a52f46714620

--- src/com/calclab/hablar/core/client/browser/BrowserFocusHandlerIE6.java
+++ src/com/calclab/hablar/core/client/browser/BrowserFocusHandlerIE6.java
@@ -1,9 +1,10 @@
 package com.calclab.hablar.core.client.browser;
 
-public class BrowserFocusHandlerIE6 {
+public class BrowserFocusHandlerIE6 extends BrowserFocusHandler {
     /**
      * Adds the focus events to the browser
      */
+    @Override
     protected native void addFocusListenerEvents() /*-{
         $wnd.document.onfocusin = function() {
         this.@com.calclab.hablar.core.client.browser.BrowserFocusHandler::setFocus(Z)(true);
