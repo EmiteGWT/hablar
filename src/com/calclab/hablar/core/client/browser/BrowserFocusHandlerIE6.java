bbe7fadc192f367fc0a35b09c03f74577996dde0
ashward.surevine <ashward.surevine@87f8d446-a148-0410-9e96-a52f46714620>
2010-08-05 17:52:53 +0000

    Attempting to fix Issue #306
    git-svn-id: https://emite.googlecode.com/svn/trunk/hablar@1541 87f8d446-a148-0410-9e96-a52f46714620

--- src/com/calclab/hablar/core/client/browser/BrowserFocusHandlerIE6.java
+++ src/com/calclab/hablar/core/client/browser/BrowserFocusHandlerIE6.java
@@ -3,6 +3,10 @@ package com.calclab.hablar.core.client.browser;
 public class BrowserFocusHandlerIE6 extends BrowserFocusHandler {
     /**
      * Adds the focus events to the browser
+     * 
+     * Thanks to
+     * http://www.sitepen.com/blog/2008/10/13/the-cross-browser-window-
+     * focus-blues/
      */
     @Override
     protected native void addFocusListenerEvents() /*-{
@@ -10,7 +14,12 @@ public class BrowserFocusHandlerIE6 extends BrowserFocusHandler {
         @com.calclab.hablar.core.client.browser.BrowserFocusHandler::changeFocus(Z)(true);
         };
         $wnd.document.onfocusout = function() {
+        if($wnd._BrowserFocusHandlerIE6ActiveElement != $wnd.document.activeElement) {
+        $wnd._BrowserFocusHandlerIE6ActiveElement = $wnd.document.activeElement;
+        } else {
         @com.calclab.hablar.core.client.browser.BrowserFocusHandler::changeFocus(Z)(false);
+        }
         };
+        $wnd._BrowserFocusHandlerIE6ActiveElement = $wnd.document.activeElement;
     }-*/;
 }
