93a5f5167258840d4bba77ef085fed61efc50f70
danigb <danigb@87f8d446-a148-0410-9e96-a52f46714620>
2010-08-14 00:55:51 +0000

    preparing compilation
    git-svn-id: https://emite.googlecode.com/svn/trunk/hablar@1546 87f8d446-a148-0410-9e96-a52f46714620

---
+++ src/com/calclab/hablar/icons/jpg/client/HablarJpgIconsEntryPoint.java
@@ -0,0 +1,11 @@
+package com.calclab.hablar.icons.jpg.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+public class HablarJpgIconsEntryPoint implements EntryPoint {
+
+    @Override
+    public void onModuleLoad() {
+    }
+
+}
