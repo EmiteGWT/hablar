2b92c0d6aeffb22d378812584390416183d8b791
Ashley Ward <ash@netality.com>
2011-08-02 09:56:43 +0100

    Updated to work with latest emite HEAD Removed the requirement for emite to have a bunch of ginjectors as these have now been removed

--- src/main/java/com/calclab/hablar/client/EmiteCompleteGinjector.java
+++ src/main/java/com/calclab/hablar/client/EmiteCompleteGinjector.java
@@ -1,20 +1,61 @@
 package com.calclab.hablar.client;
 
-import com.calclab.emite.browser.client.BrowserGinjector;
-import com.calclab.emite.core.client.CoreGinjector;
-import com.calclab.emite.im.client.ImGinjector;
-import com.calclab.emite.reconnect.client.ReconnectGinjector;
-import com.calclab.emite.xep.avatar.client.AvatarGinjector;
-import com.calclab.emite.xep.chatstate.client.ChatStateGinjector;
-import com.calclab.emite.xep.disco.client.DiscoveryGinjector;
-import com.calclab.emite.xep.muc.client.MucGinjector;
-import com.calclab.emite.xep.mucchatstate.client.MucChatStateGinjector;
-import com.calclab.emite.xep.mucdisco.client.MucDiscoveryGinjector;
-import com.calclab.emite.xep.search.client.SearchGinjector;
-import com.calclab.emite.xep.storage.client.PrivateStorageGinjector;
-import com.calclab.emite.xep.vcard.client.VCardGinjector;
+import com.calclab.emite.browser.client.BrowserModule;
+import com.calclab.emite.core.client.CoreModule;
+import com.calclab.emite.core.client.conn.XmppConnection;
+import com.calclab.emite.core.client.xmpp.session.XmppSession;
+import com.calclab.emite.im.client.ImModule;
+import com.calclab.emite.im.client.chat.ChatManager;
+import com.calclab.emite.im.client.presence.PresenceManager;
+import com.calclab.emite.im.client.roster.SubscriptionHandler;
+import com.calclab.emite.im.client.roster.XmppRoster;
+import com.calclab.emite.xep.chatstate.client.ChatStateModule;
+import com.calclab.emite.xep.chatstate.client.StateManager;
+import com.calclab.emite.xep.disco.client.DiscoveryModule;
+import com.calclab.emite.xep.muc.client.MucModule;
+import com.calclab.emite.xep.muc.client.RoomManager;
+import com.calclab.emite.xep.mucchatstate.client.MUCChatStateManager;
+import com.calclab.emite.xep.mucchatstate.client.MucChatStateModule;
+import com.calclab.emite.xep.mucdisco.client.MucDiscoveryModule;
+import com.calclab.emite.xep.mucdisco.client.RoomDiscoveryManager;
+import com.calclab.emite.xep.search.client.SearchManager;
+import com.calclab.emite.xep.search.client.SearchModule;
+import com.calclab.emite.xep.storage.client.PrivateStorageManager;
+import com.calclab.emite.xep.storage.client.PrivateStorageModule;
+import com.calclab.emite.xep.vcard.client.VCardManager;
+import com.calclab.emite.xep.vcard.client.VCardModule;
+import com.google.gwt.inject.client.GinModules;
+import com.google.gwt.inject.client.Ginjector;
 
-public interface EmiteCompleteGinjector extends CoreGinjector, ChatStateGinjector, ImGinjector, ReconnectGinjector,
-	AvatarGinjector, DiscoveryGinjector, MucGinjector, MucChatStateGinjector, MucDiscoveryGinjector,
-	SearchGinjector, PrivateStorageGinjector, VCardGinjector, BrowserGinjector {
+@GinModules({ CoreModule.class, ImModule.class, MucModule.class,
+		MucChatStateModule.class, ChatStateModule.class,
+		PrivateStorageModule.class, MucDiscoveryModule.class,
+		VCardModule.class, SearchModule.class, DiscoveryModule.class,
+		BrowserModule.class })
+public interface EmiteCompleteGinjector extends Ginjector {
+	XmppSession getXmppSession();
+
+	XmppConnection getXmppConnection();
+
+	XmppRoster getXmppRoster();
+
+	ChatManager getChatManager();
+
+	RoomManager getRoomManager();
+
+	StateManager getStateManager();
+
+	MUCChatStateManager getMUCChatStateManager();
+
+	PresenceManager getPresenceManager();
+
+	SubscriptionHandler getSubscriptionHandler();
+
+	PrivateStorageManager getPrivateStorageManager();
+
+	RoomDiscoveryManager getRoomDiscoveryManager();
+
+	VCardManager getVCardManager();
+
+	SearchManager getSearchManager();
 }
